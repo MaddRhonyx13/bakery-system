297beeb5aa9e1287e6c71cc51dca34783ff6f6e0
MaddRhonyx13 <tummiebabbie6@gmail.com>
2025-11-02 23:51:44 +0200

    Update api.js

--- client/frontend/src/services/api.js
+++ client/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+const API_BASE = process.env.REACT_APP_API_URL || 'https://bakery-system-4.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
